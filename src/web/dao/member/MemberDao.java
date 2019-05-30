718386913ada287bc654e748ddd4772e3124660d
yjayo0124 <seonhong5388@gmail.com>
2019-05-30 14:10:58 +0900

    공지사항게시판 수정및 관리자 구분코드생성
    비횐/일반회원 // 관리자 인지확인하는 코드 생성

--- src/web/dao/member/MemberDao.java
+++ src/web/dao/member/MemberDao.java
@@ -9,11 +9,15 @@ public interface MemberDao {
 	public MemberDetail selectMemberByMemberid(MemberDetail member);
 
 	/**
-	 * 회원가입
+	 * �쉶�썝媛��엯
 	 * 
-	 * @param member - 회원가입 정보
+	 * @param member - �쉶�썝媛��엯 �젙蹂�
 	 */
 	public void insert(MemberDetail member);
 
 
+	//관리자인지 일반사용자인지 확인하는 메소드
+	//리턴값이 1이면 관리자 / 0이면 일반사용자!!!
+	public int checkMemberCode(int member_code);
+	
 }

adc6e6ab3e8459c1b8d180e57a2a81f010942668
choi hyang keun <gidrms99@naver.com>
2019-06-14 14:40:38 +0900

    123cc
    123

---
+++ src/web/controller/board/sell/PaymentController.java
@@ -0,0 +1,21 @@
+package web.controller.board.sell;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/pay")
+public class PaymentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		request.getRequestDispatcher("/sell/view").forward(request, response);
+	}
+
+}
